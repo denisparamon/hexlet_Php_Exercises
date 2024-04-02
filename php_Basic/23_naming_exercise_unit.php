df6050b162e3e9761d88f765d073e06497bd381e
Hooman <denis_paramon@icloud.com>
2024-04-02 09:28:19 +0500

    addingThe23rdExercise

---
+++ php_Basic/23_naming_exercise_unit.php
@@ -0,0 +1,18 @@
+<?php
+
+//const DAY = 6;
+//const MOUNT = 5;
+//const YEAR = 1987;
+// $day = 6;
+// $mount = 5;
+// $year = 1987;
+
+function getFormattedBirthday($day, $mount, $year)
+{
+    $x = str_pad($day, 2, '0', STR_PAD_LEFT);
+    $y = sprintf("%s-%02d-%d", $x, $mount, $year);
+    return $y;
+}
+
+// $result = getFormattedBirthday($day, $mount, $year);
+// print_r($result);
