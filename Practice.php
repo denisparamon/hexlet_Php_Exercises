9a60204467233c5b12667390bc9910fed4af1e18
Hooman <denis_paramon@icloud.com>
2024-04-11 13:40:35 +0500

    addPracticeFile

---
+++ Practice.php
@@ -0,0 +1,51 @@
+<?php
+
+//function chtototam($text)
+//{
+//    $result = '';
+//    $long = mb_strlen($text);
+//    for ($i = 0; $i < $long; $i++) {
+//        $ch = mb_substr($text, $i, 1);
+//        if (mb_strtolower($ch) === $ch) {
+//            $result .= mb_strtoupper($ch);
+//        } else {
+//            $result .= mb_strtolower($ch);
+//        }
+//    }
+//    return $result;
+//}
+//
+//echo chtototam("LolkaPidr");
+
+////mb_strlen Получает длину строки
+////mb_substr Возвращает часть строки
+////mb_strtolower Приводит строку к нижнему регистру
+////mb_strtoupper Приведит строку к верхнему регистру
+
+//Function summa($summ)
+//{
+//    $result = 0;
+//    $i = 0;
+//    $long = strlen($summ);
+//    while ($i < $long) {
+//    $result += $summ[$i];
+//    $i++;
+//    }
+//    return $result;
+//}
+//
+//$result = summa('123456789');
+//echo $result;
+
+//function summa($summ)
+//{
+//    $result = 0;
+//    $long = strlen($summ);
+//    for($i = 0; $i < $long; $i++) {
+//    $result += $summ[$i];
+//    }
+//    return $result;
+//}
+//
+//$result = summa('123456789');
+//echo $result;
