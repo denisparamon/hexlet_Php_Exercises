b6e08352745155d4a13634bea9f12db4abd0b7e2
Hooman <denis_paramon@icloud.com>
2024-04-18 13:09:51 +0500

    adding38th

---
+++ php_Basic/38_nested-namespaces_exercise_unit.php
@@ -0,0 +1,18 @@
+<?php
+//NS.php:
+
+namespace App\Internal\NS;
+
+function hello()
+{
+    return 'ns';
+}
+
+//Solution.php:
+
+namespace App\Solution;
+
+function hello()
+{
+    return 'solution';
+}
