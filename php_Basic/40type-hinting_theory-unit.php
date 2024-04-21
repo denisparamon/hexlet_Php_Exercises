65f6026c6aa4a7b0a43e0f66ca96b1aaad533e7b
Hooman <denis_paramon@icloud.com>
2024-04-21 09:19:45 +0500

    adding40th

---
+++ php_Basic/40type-hinting_theory-unit.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Number;
+
+function reverse(int $number)
+{
+    $isNegative = $number < 0; // Проверяем, является ли число отрицательным
+    $number = abs($number); // Преобразуем число к положительному виду
+    $numberStr = (string) $number; // Преобразуем число в строку
+    $reversStr = strrev($numberStr); // Переворачиваем строку
+    $reversNumber = (int) $reversStr; // Преобразуем обратно в число
+    if ($isNegative) { // Если было отричательным то возвращаем его обратно в отцальное
+        $reversNumber *= -1;
+    }
+    return $reversNumber;
+}
