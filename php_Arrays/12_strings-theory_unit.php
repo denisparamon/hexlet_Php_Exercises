86be447bc14d539c10541321f8e59b3710c07d4b
Hooman <denis_paramon@icloud.com>
2024-05-11 11:43:36 +0500

    Eddind12Th

--- php_Arrays/12_strings-theory_unit.php
+++ php_Arrays/12_strings-theory_unit.php
@@ -5,9 +5,23 @@ namespace App\Strings;
 function makeCensored($text, $stopWords)
 {
     $words = explode(' ', $text);
-    $censoredWords = [];
+    $result = [];
     foreach ($words as $word) {
-        $censoredWords[] = in_array($word, $stopWords) ? '$#%!' : $word;
+        $cresult[] = in_array($word, $stopWords) ? '$#%!' : $word;
     }
-    return implode(' ', $censoredWords);
+    return implode(' ', $result);
 }
+
+////Решение Хекслет
+//// BEGIN
+//function makeCensored(string $text, array $stopWords)
+//{
+//    $words = explode(' ', $text);
+//    $result = [];
+//    foreach ($words as $word) {
+//        $result[] = in_array($word, $stopWords) ? '$#%!' : $word;
+//    }
+//
+//    return implode(' ', $result);
+//}
+//// END
