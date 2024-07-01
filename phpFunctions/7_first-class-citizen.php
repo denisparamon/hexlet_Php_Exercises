740d273cbbf668ebf87e7db01297f43d7a4fb250
Hooman <denis_paramon@icloud.com>
2024-07-01 10:19:55 +0500

    adding7th

---
+++ phpFunctions/7_first-class-citizen.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Strings;
+
+function run(string $text)
+{
+    $last = function($text) {
+        if ($text === '') {
+            return null;
+        }
+        return $text[strlen($text) - 1];
+    };
+
+    return $last($text);
+}
