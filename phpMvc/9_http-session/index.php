883711260c50287300b6a1109cf57cab3d6a9830
Hooman <denis_paramon@icloud.com>
2024-11-05 11:41:04 +0500

    adding9http-session

---
+++ phpMvc/9_http-session/index.php
@@ -0,0 +1,20 @@
+<?php
+
+use Slim\Factory\AppFactory;
+
+require '/composer/vendor/autoload.php';
+
+$companies = App\Generator::generate(100);
+
+$app = AppFactory::create();
+$app->addErrorMiddleware(true, true, true);
+
+$app->get('/', function ($request, $response) {
+    return $response->write('go to the /companies');
+});
+
+// BEGIN (write your solution here)
+
+// END
+
+$app->run();
