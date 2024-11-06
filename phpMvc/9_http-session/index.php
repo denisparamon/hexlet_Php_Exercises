224df3e22fbe6ea8c0dc0769d199ed7d788577f3
Hooman <denis_paramon@icloud.com>
2024-11-06 16:15:02 +0500

    edding9http-session

--- phpMvc/9_http-session/index.php
+++ phpMvc/9_http-session/index.php
@@ -13,8 +13,20 @@ $app->get('/', function ($request, $response) {
     return $response->write('go to the /companies');
 });
 
-// BEGIN (write your solution here)
+// BEGIN
+$app->get('/companies', function ($request, $response) use ($companies) {
+    // Получаем параметры запроса
+    $page = (int) ($request->getQueryParams()['page'] ?? 1);
+    $per = (int) ($request->getQueryParams()['per'] ?? 5);
 
+    // Вычисляем индексы для среза
+    $offset = ($page - 1) * $per;
+    $pagedCompanies = array_slice($companies, $offset, $per);
+
+    // Возвращаем данные в формате JSON
+    $response->getBody()->write(json_encode($pagedCompanies));
+    return $response->withHeader('Content-Type', 'application/json');
+});
 // END
 
 $app->run();
